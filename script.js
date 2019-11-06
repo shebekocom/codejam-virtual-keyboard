f284fc12bb9e9a5923bcb7b655fcf32815ac6f35
nshebeko <nikolay.shebeko@gmail.com>
2019-11-06 16:53:54 +0300

    feat: add hover on keybord and adding to input by click

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 // eng key lang
 
 const keyboardEng = [
-  ['`~', '1!', '2@', '3#', '4$', '5%', '6^', '7&', '8*', '9(', '0)', '-_', '=+', 'backsp'],
-  ['tab', 'qQ', 'wW', 'eE', 'rR', 'tT', 'yY', 'uU', 'iI', 'oO', 'pP', '[{', ']}', '\/|'],
-  ['caps', 'aA', 'sS', 'dD', 'fF', 'gG', 'hH', 'jJ', 'kK', 'lL', ';:', "'\"", 'enter'],
-  ['shift', 'zZ', 'xX', 'cC', 'vV', 'bB', 'nN', 'mM', ',<', '.>', '/?', 'shift'],
-  ['ctrl', 'fun', 'alt', 'space', 'alt', 'fun', 'ctrl']
+  ['`~', '1!', '2@', '3#', '4$', '5%', '6^', '7&', '8*', '9(', '0)', '-_', '=+', 
+  {
+    key: 'Backspace',
+    name: 'backsp'
+  }],
+  [{
+    key: 'Tab',
+    name: 'tab'
+  }, 'qQ', 'wW', 'eE', 'rR', 'tT', 'yY', 'uU', 'iI', 'oO', 'pP', '[{', ']}', '\/|'],
+  [{
+    key: 'CapsLock',
+    name: 'caps'
+  }, 'aA', 'sS', 'dD', 'fF', 'gG', 'hH', 'jJ', 'kK', 'lL', ';:', "'\"", {
+    key: 'Enter',
+    name: 'enter'
+  }],
+  [{
+    key: 'Shift',
+    name: 'shift'
+  }, 'zZ', 'xX', 'cC', 'vV', 'bB', 'nN', 'mM', ',<', '.>', '/?', {
+    key: 'Shift',
+    name: 'shift'
+  }],
+  [{
+    key: 'Control',
+    name: 'ctrl'
+  }, {
+    key: 'F1',
+    name: 'fn'
+  }, {
+    key: 'Alt',
+    name: 'alt'
+  }, {
+    key: ' ',
+    name: 'space'
+  }, {
+    key: 'Alt',
+    name: 'alt'
+  }, {
+    key: 'F1',
+    name: 'fn'
+  }, {
+    key: 'Control',
+    name: 'ctrl'
+  }]
 ];
 
 // rus key lang
@@ -43,7 +83,7 @@ for (let j = 0; j < keyboard.length; j++) {
     divButton.className = 'keybutton';
 
     // change style for keyButtons
-    switch (keyboard[j][i]) {
+    switch (keyboard[j][i].name) {
       case 'backsp':
         divButton.className += ' backsp';
         break;
@@ -75,11 +115,44 @@ for (let j = 0; j < keyboard.length; j++) {
 
     // add on HTML document text on buttons
     if (keyboard[j][i].length <= 2) {
-      divButton.innerHTML += `<span class="text">${keyboard[j][i][0]}</span><span class="textup">${keyboard[j][i][1]}</span>`;
+      divButton.innerHTML += `<span class="text" data-key="${keyboard[j][i][0]}">${keyboard[j][i][0]}</span><span class="textup" data-key="${keyboard[j][i][1]}">${keyboard[j][i][1]}</span>`;
     } else {
-      divButton.innerHTML += `<span class="functionButton">${keyboard[j][i]}</span>`;
+      divButton.innerHTML += `<span class="functionButton" data-key="${keyboard[j][i].key}">${keyboard[j][i].name}</span>`;
     }
     divRow.append(divButton);
   }
   keyBoard.append(divRow);
 }
+
+
+// hover buttons on virtual keybord
+textarea.addEventListener('keydown', () => {
+  const element = document.querySelector('[data-key="' + event.key + '"]');
+  element.parentElement.classList.add('hover');
+});
+
+textarea.addEventListener('keyup', () => {
+  const element = document.querySelector('[data-key="' + event.key + '"]');
+  element.parentElement.classList.remove('hover');
+});
+
+
+// adding to input by click translation
+
+keyBoard.addEventListener('mousedown', () => {
+  let element = '';
+  if (event.target.className === 'keyboard' || event.target.className === 'row') return;
+  if (event.target.tagName === "SPAN") {
+    element = event.target.parentElement;
+  } else {
+    element = event.target;
+  }
+  element.classList.add('hover');
+  textarea.innerHTML += element.innerText;
+  console.dir(element.className);
+})
+
+document.addEventListener('mouseup', () => {
+  const keybutton = document.querySelectorAll('.hover');
+  keybutton.forEach((el) => el.classList.remove('hover'));
+});
